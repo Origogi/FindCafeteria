513c14263719175f571af467d3f3ce64dff15bb6
gimjeongtae <rlawjdxo1212@naver.com>
2018-12-05 21:04:28 +0900

    Second commit

--- app/src/main/java/com/cafeteria/free/findcafeteria/view/MainActivity.java
+++ app/src/main/java/com/cafeteria/free/findcafeteria/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.cafeteria.free.findcafeteria;
+package com.cafeteria.free.findcafeteria.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.cafeteria.free.findcafeteria.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        
     }
 }
