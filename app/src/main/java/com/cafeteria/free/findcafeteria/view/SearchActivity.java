60682cad6ab7574bbbfbcffd57c69822c7e05115
gimjeongtae <rlawjdxo1212@naver.com>
2019-01-03 20:28:27 +0900

    Fix gradle library

--- app/src/main/java/com/cafeteria/free/findcafeteria/view/SearchActivity.java
+++ app/src/main/java/com/cafeteria/free/findcafeteria/view/SearchActivity.java
@@ -1,18 +1,15 @@
 package com.cafeteria.free.findcafeteria.view;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cafeteria.free.findcafeteria.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(MainActivity.this, TestActivity.class));
+        setContentView(R.layout.activity_search);
     }
 }
