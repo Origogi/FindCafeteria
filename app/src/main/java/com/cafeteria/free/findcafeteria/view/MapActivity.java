143e98dee33f52ed36d514e6d947acef210b0221
biny <noobiny@daum.net>
2019-01-03 20:55:10 +0900

    Add MapActivity Add NoActionBar

---
+++ app/src/main/java/com/cafeteria/free/findcafeteria/view/MapActivity.java
@@ -0,0 +1,21 @@
+package com.cafeteria.free.findcafeteria.view;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cafeteria.free.findcafeteria.R;
+import com.cafeteria.free.findcafeteria.databinding.ActivityMapBinding;
+
+public class MapActivity extends AppCompatActivity {
+
+    ActivityMapBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_map);
+        binding.setActivity(this);
+
+    }
+
+}
