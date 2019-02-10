337a0abf889b8002c9a4ffaa062e9510ad1d40ee
biny <noobiny@daum.net>
2019-02-10 15:50:24 +0900

    DetailActivity : CircleIndicator 추가

---
+++ app/src/main/java/com/cafeteria/free/findcafeteria/util/ImageSliderAdapter.java
@@ -0,0 +1,63 @@
+package com.cafeteria.free.findcafeteria.util;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.RequestManager;
+import com.cafeteria.free.findcafeteria.R;
+
+import java.util.ArrayList;
+
+public class ImageSliderAdapter extends PagerAdapter {
+
+    Context mContext;
+    LayoutInflater mLayoutInflater;
+    ArrayList<Integer> mResources;
+    private final RequestManager glide;
+
+    public ImageSliderAdapter(Context context, ArrayList<Integer> mResources, RequestManager glide) {
+        this.glide = glide;
+        mContext = context;
+        this.mResources = mResources;
+        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+
+    @Override
+    public int getCount() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        View itemView = mLayoutInflater.inflate(R.layout.viewpager_image, container, false);
+
+        int realPos = position % mResources.size();
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.img_viewpager_childimage);
+        glide.load(mResources.get(realPos)).placeholder(R.drawable.loadingimage).error(R.drawable.loadingimage).into(imageView);
+
+        container.addView(itemView, 0);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+
+}
--- app/src/main/java/com/cafeteria/free/findcafeteria/view/DetailActivity.java
+++ app/src/main/java/com/cafeteria/free/findcafeteria/view/DetailActivity.java
@@ -1,15 +1,30 @@
 package com.cafeteria.free.findcafeteria.view;
 
 import android.databinding.DataBindingUtil;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
 import com.cafeteria.free.findcafeteria.R;
 import com.cafeteria.free.findcafeteria.databinding.ActivityDetailBinding;
+import com.cafeteria.free.findcafeteria.util.ImageSliderAdapter;
+
+import java.util.ArrayList;
+
+import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
 
 public class DetailActivity extends AppCompatActivity {
 
     ActivityDetailBinding binding;
+
+    private ArrayList<Integer> images = new ArrayList<>();
+    private int dotsCount;
+    private ImageView[] dots;
+    private ImageSliderAdapter imageSliderAdapter;
+    private int currentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +32,60 @@ public class DetailActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_detail);
         binding.setActivity(this);
 
+        imageSliderAdapter = new ImageSliderAdapter(this, images, Glide.with(this));
+
+        // TODO: 2019-02-10 파이어베이스에 저장된 URL경로를 가져오는 걸로 변경 (Integer -> String)
+        images.add(R.drawable.common_google_signin_btn_icon_dark);
+        images.add(R.drawable.common_full_open_on_phone);
+        images.add(R.drawable.common_google_signin_btn_icon_dark_normal_background);
+        images.add(R.drawable.loadingimage);
+
+        binding.homeslider.setAdapter(imageSliderAdapter);
+
+        setUiPageViewController();
+    }
+
+    private void setUiPageViewController() {
+
+        binding.homeslider.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPosition = position;
+                for (int i = 0; i < dotsCount; i++) {
+                    dots[i].setImageDrawable(getResources().getDrawable(R.drawable.nonselecteditem_dot));
+
+                }
+                if (dots != null) {
+                    dots[currentPosition % 5].setImageDrawable(getResources().getDrawable(R.drawable.selecteditem_dot));
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        dotsCount = images.size();
+        dots = new ImageView[dotsCount];
+
+        for (int i = 0; i < dotsCount; i++) {
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(getResources().getDrawable(R.drawable.nonselecteditem_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.setMargins(4, 0, 4, 0);
+
+            binding.viewPagerCountDots.addView(dots[i], params);
+        }
+        dots[0].setImageDrawable(getResources().getDrawable(R.drawable.selecteditem_dot));
     }
 }
