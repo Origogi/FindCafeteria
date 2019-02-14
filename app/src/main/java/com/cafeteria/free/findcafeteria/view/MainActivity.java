271d535ce5e4c8c4d83a58d589a041125ab70dab
gimjeongtae <rlawjdxo1212@naver.com>
2019-02-14 21:07:16 +0900

    add loading

--- app/src/main/java/com/cafeteria/free/findcafeteria/view/MainActivity.java
+++ app/src/main/java/com/cafeteria/free/findcafeteria/view/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(MainActivity.this, SearchActivity.class));
+        startActivity(new Intent(MainActivity.this, LoadingActivity.class));
     }
 }
