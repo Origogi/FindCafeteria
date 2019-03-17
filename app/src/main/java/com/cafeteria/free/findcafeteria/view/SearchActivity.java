b0ef9e9dd1f37a3e8c0d7d25d4043af75a3e7795
Administrator <rlawjdxo1212@naver.com>
2019-03-17 22:42:34 +0900

    Fix scroll bug

--- app/src/main/java/com/cafeteria/free/findcafeteria/view/SearchActivity.java
+++ app/src/main/java/com/cafeteria/free/findcafeteria/view/SearchActivity.java
@@ -1,27 +1,33 @@
 package com.cafeteria.free.findcafeteria.view;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.provider.SearchRecentSuggestions;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.*;
 import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.SearchView;
 
 import com.cafeteria.free.findcafeteria.R;
 import com.cafeteria.free.findcafeteria.model.CafeteriaData;
 import com.cafeteria.free.findcafeteria.model.CafeteriaDataProvider;
+import com.cafeteria.free.findcafeteria.model.MySuggestionProvider;
 import com.cafeteria.free.findcafeteria.util.Logger;
 
 import java.util.List;
 
 import io.reactivex.Maybe;
 import io.reactivex.observers.DisposableMaybeObserver;
+
+
 public class SearchActivity extends AppCompatActivity {
 
 
@@ -31,6 +37,8 @@ public class SearchActivity extends AppCompatActivity {
     FloatingActionButton mapFab;
 
     GestureDetector gestureDetector;
+
+    String currentQuery;
 
     RecyclerView.OnItemTouchListener itemTouchListener = new RecyclerView.OnItemTouchListener() {
         @Override
@@ -52,31 +60,19 @@ public class SearchActivity extends AppCompatActivity {
         public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) { }
 
     };
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Logger.d("");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_activity_actions, menu);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(),
-                LinearLayoutManager.VERTICAL, false);
-        layoutManager.setAutoMeasureEnabled(true);
-
-        recyclerView = findViewById(R.id.recycle_view);
-        recyclerView.setLayoutManager(layoutManager);
-
-        recyclerViewAdapter = new RecyclerViewAdapter(this);
-        recyclerView.setAdapter(recyclerViewAdapter);
-
-        gestureDetector= new GestureDetector(getApplicationContext(), new GestureDetector.SimpleOnGestureListener() {
-            @Override
-            public boolean onSingleTapUp(MotionEvent e) {
-                return true;
-            }
-        });
-
-        recyclerView.addOnItemTouchListener(itemTouchListener);
-        searchView = findViewById(R.id.searchView);
-
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -116,6 +112,39 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        //setIntent(intent);
+        handleIntent(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(),
+                LinearLayoutManager.VERTICAL, false);
+        layoutManager.setAutoMeasureEnabled(true);
+
+        recyclerView = findViewById(R.id.recycle_view);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerViewAdapter = new RecyclerViewAdapter(this);
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        gestureDetector= new GestureDetector(getApplicationContext(), new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+        });
+
+        recyclerView.addOnItemTouchListener(itemTouchListener);
+
         mapFab = findViewById(R.id.map_fab);
         mapFab.setVisibility(View.GONE);
 
@@ -130,6 +159,12 @@ public class SearchActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+        Toolbar toolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+
+        handleIntent(getIntent());
     }
 
     private void updateRecycleView(List<CafeteriaData> list) {
@@ -161,6 +196,22 @@ public class SearchActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MapActivity.class);
         intent.putExtra("query", query);
         startActivity(intent);
+    }
 
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            if (query.equals(currentQuery)) {
+                return;
+            }
+            currentQuery = query;
+            Logger.d("query=" + query);
+
+            SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
+                    MySuggestionProvider.AUTHORITY, MySuggestionProvider.MODE);
+            suggestions.saveRecentQuery(currentQuery, null);
+            searchView.setQuery(currentQuery, true);
+        }
     }
 }
